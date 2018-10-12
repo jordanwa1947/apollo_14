ecf69aa3ffe20907dd17cb93c66e7d021b262443
Jordan Whitten <jordanwa1947@gmail.com>
2018-10-12 11:10:35 -0600

    finished average age class method and testing

--- spec/features/user_sees_astronauts_spec.rb
+++ spec/features/user_sees_astronauts_spec.rb
@@ -7,12 +7,22 @@ describe 'astronaut index' do
     mission = astronaut_1.missions.create(title: "Secret", time_in_space: 1600)
 
     visit '/astronauts'
-    save_and_open_page
+
     expect(page).to have_content("Name: #{astronaut_1.name}")
     expect(page).to have_content("Name: #{astronaut_2.name}")
     expect(page).to have_content("Age: #{astronaut_1.age}")
     expect(page).to have_content("Age: #{astronaut_2.age}")
     expect(page).to have_content("Job: #{astronaut_1.job}")
     expect(page).to have_content("Job: #{astronaut_2.job}")
+  end
+
+  it 'user can see astronauts' do
+    astronaut_1 = Astronaut.create(name: "Astronaut 1", age: 43, job: "Engineer")
+    astronaut_2 = Astronaut.create(name: "Astronaut 2", age: 54, job: "Engineer")
+    mission = astronaut_1.missions.create(title: "Secret", time_in_space: 1600)
+
+    visit '/astronauts'
+
+    expect(page).to have_content("average_age: #{Astronaut.average_age}")
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,14 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'agerage_age class method' do
+    it 'should calculate the average age of astronauts' do
+      astronaut_1 = Astronaut.create(name: "Astronaut 1", age: 51, job: "Engineer")
+      astronaut_2 = Astronaut.create(name: "Astronaut 2", age: 49, job: "Engineer")
+
+
+      expect(Astronaut.average_age).to eq 50
+    end 
+  end
 end
